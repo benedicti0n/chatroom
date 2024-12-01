e5ed6ee5f3cd96406bd307591ad679af93094735
Ashesh Bandopadhyay <lbandopadhyay2@gmail.com>
2024-12-01 05:45:36 +0530

    react router dom implement

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Home from './components/Home'
+import Room from './components/Room'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className='h-min-screen w-full flex justify-center items-center'>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path="/:roomId" element={<Room />} />
+      </Routes>
+    </div>
   )
 }
 
